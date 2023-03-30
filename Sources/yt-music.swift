1da980246a6d723f615c4614c596a48cecc1654e
Ranoiaetep <ranoiaetep@me.com>
2023-03-29 20:47:51 -0400

    - Rewrite shell method to call `yt-dlp` directly. - Add more detailed help messages for each arguments. - Add more options includes: `--music-format`, `--verbose`, `--homebrew-directory`. - Adjust behavior when the link is a video within a playlist to download only the individual video, instead of the entire playlist

--- Sources/yt-music.swift
+++ Sources/yt-music.swift
@@ -1,69 +1,152 @@
 import Foundation
 import ArgumentParser
+import System
 
 @main
 struct ytMusic: ParsableCommand {
-    @Argument(
-        help: "Youtube link(s) to download"
+    static var configuration: CommandConfiguration = .init(
+        abstract: "Download Youtube Videos as Music",
+        discussion: """
+        Download videos by simply copying each links: `yt-music "link1..." "link2..."`.
+        - Support download each videos individually, or download the entire playlist.
+        - Multiple links can be appended.
+        """,
+        version: "0.0.2"
     )
-    var links: [URL]
-    /// [https://www.youtube.com/watch?v=4uqONJT6G-M, https://www.youtube.com/watch?v=4bE7use7uQc]
 
+    @Argument(
+        help: .init(
+            "Youtube url(s) to download",
+            discussion: """
+            Both individual links and playlist links are supported.
+
+            """
+        )
+    )
+    var urls: [YoutubeURL]
+    
     @Option(
-        name: [.long, .customShort("o")],
-        help: "Location to save your download",
+        name: [.short, .customLong("output")],
+        help: .init(
+            "Provide download location",
+            discussion: """
+            Provide a custom directory, or user's default download folder will be used.
+
+            """,
+            valueName: "dir"
+        ),
         completion: .directory
     )
-    var directory: URL = .downloadsDirectory
+    var outputDirectory: URL = .downloadsDirectory
+
+    @Option(
+        name: .shortAndLong,
+        help: .init(
+            "Choose custom music format",
+            discussion: """
+            Current available formats: m4a, mp3
+
+            """,
+            valueName: "ext"
+        )
+    )
+    var format: musicFormat = .m4a
+
+    @Flag(
+        name: .shortAndLong,
+        help: "Display extra information while processing."
+    )
+    var verbose: Bool = false
+
+    @Option(
+        name: [.customLong("custom-homebrew")],
+        help: .init(
+            "Choose custom Homebrew location",
+            valueName: "dir",
+            visibility: .hidden
+        ),
+        completion: .directory
+    )
+    var homebrewDirectory: URL = defaultHomebrewDirectory
+
+    static var defaultHomebrewDirectory: URL = {
+        let process = Process()
+        let pipe = Pipe()
+        process.executableURL = URL(filePath: "/bin/zsh")!
+        process.arguments = ["-c", "brew --prefix"]
+        process.standardOutput = pipe
+        try! process.run()
+
+        let urlString = String(data: try! pipe.fileHandleForReading.readToEnd()!, encoding: .ascii)!.trimmingCharacters(in: .newlines)
+
+        return URL(filePath: urlString.trimmingCharacters(in: .whitespaces)).appending(path: "bin", directoryHint: .isDirectory)
+    }()
+
+    @Flag(
+        name: .customLong("download"),
+        inversion: .prefixedNo,
+        exclusivity: .exclusive,
+        help: .init(
+            "Toggle if the command will perform downloads",
+            discussion: """
+            This flag should be used only for debug purposes
+
+            """,
+            visibility: .hidden
+        )
+    )
+    var shouldDownload: Bool = true
 
     func run() throws {
-        for (index, link) in links.enumerated() {
-            if links.count > 1 {
-                print("Task \(index + 1) of \(links.count): ", terminator: "")
+        let ytDlp = URL(filePath: "yt-dlp", relativeTo: homebrewDirectory).absoluteURL
+        let ffmpeg = URL(filePath: "ffmpeg", relativeTo: homebrewDirectory).absoluteURL
+
+        for (index, url) in urls.enumerated() {
+            if urls.count > 1 {
+                print("Task \(index + 1) of \(urls.count): ", terminator: "")
             }
-            print("Downloading from \(link.absoluteString.quoted)...")
 
-            let baseScript = """
-            yt-dlp \(link.absoluteString.quoted) --paths \(directory.path().quoted) --embed-thumbnail --embed-metadata --embed-chapters --quiet --progress --newline --print "
-            %(autonumber)02d. %(title)s.%(ext)s" --no-simulate -f m4a
-            """
-
-            shell(baseScript, taskIndex: links.count > 1 ? index + 1: nil)
+            if shouldDownload {
+                print("Downloading from \(url.v ?? url.list ?? "")...")
+                let process = try! Process.run(ytDlp, arguments: [
+                    (url.v ?? url.list)!,
+                    "--paths", outputDirectory.path(),
+                    "--embed-thumbnail", "--embed-metadata", "--embed-chapters",
+                    "--progress", "--newline", verbose ? "--verbose" : "",
+                    "--no-simulate",
+                    "--print", "\n%(autonumber)02d. %(title)s.%(ext)s",
+                    "-f", format.rawValue,
+                    "--ffmpeg-location", ffmpeg.path()
+                ].filter{ $0.count > 0 }, terminationHandler: { _ in
+                    print("\nTask \(urls.count > 1 ? (index + 1).description + " " : "")Finished!\n")
+                })
+                process.waitUntilExit()
+                print(String.init(repeating: "-", count: 40), "\n")
+            }
+            else {
+                print(url)
+            }
         }
     }
 
     func validate() throws {
-        guard links.count >= 1 else {
-            throw ValidationError("You must add at least 1 link")
+        guard urls.count >= 1 else {
+            throw ValidationError("You must add at least 1 url")
         }
     }
-}
 
+    enum musicFormat: String, ExpressibleByArgument {
+        case m4a
+        case mp3
+    }
+}
 
 extension URL: ExpressibleByArgument {
     public init?(argument: String) {
-        guard let url = Self.init(string: argument) else {
-            return nil
-        }
-        self = url
+        self.init(string: argument)
     }
 
     public var defaultValueDescription: String {
-        return "~/Download".quoted
-    }
-}
-
-func shell(_ command: String, taskIndex: Int? = nil) {
-    let process = try! Process.run(URL(filePath: "/bin/zsh"), arguments: ["-c", command]) { process in
-        print("\nTask \(taskIndex?.description.appending(" ") ?? "")Finished!\n")
-    }
-
-    process.waitUntilExit()
-    print(String.init(repeating: "-", count: 40), "\n")
-}
-
-extension String {
-    var quoted: String {
-        return "\"\(self)\""
+        self.path()
     }
 }
