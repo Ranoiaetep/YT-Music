e22af0608a1ddd55214564e96a38c4007f007f51
Ranoiaetep <ranoiaetep@me.com>
2023-03-29 20:35:15 -0400

    Creating `YoutubeURL` type as a separate type from `URL`

---
+++ Sources/YoutubeURL.swift
@@ -0,0 +1,43 @@
+//
+//  YoutubeURL.swift
+//  ytMusic
+//
+//  Created by Peter Cong on 3/29/23.
+//
+
+import Foundation
+import ArgumentParser
+
+struct YoutubeURL {
+    let v: String?
+    let list: String?
+    let index: Int?
+}
+
+extension YoutubeURL: ExpressibleByArgument {
+    init?(argument: String) {
+        guard let url = URL(string: argument),
+              let components = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
+            return nil
+        }
+        guard let host = components.host,
+              host.contains("youtube") else { return nil }
+        guard let query = components.queryItems,
+              query.contains(where: { item in
+                  item.name == "v" || item.name == "list"
+              }) else {
+            return nil
+        }
+        self.v = query.first{ $0.name == "v" }?.value
+        self.list = query.first{ $0.name == "list" }?.value
+        self.index = Int(query.first{ $0.name == "index" }?.value ?? "")
+    }
+}
+
+extension YoutubeURL: Encodable {}
+
+extension YoutubeURL: CustomStringConvertible {
+    var description: String {
+        String(decoding: try! JSONEncoder().encode(self), as: UTF8.self)
+    }
+}
